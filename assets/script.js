a6539ec8215b16d11dc1f93b1aff231df326dde7
mrmiles68 <cheaptechsupport2000@gmail.com>
2021-03-01 20:46:46 -0800

    added variables

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,8 @@
-const date =document.querySelector('#date')
-date.textcontent=moment().format("MMM Do, YY");
+const date = document.querySelector('#date')
+date.textcontent = moment().format("MMM Do, YY");
+var projectName = ""
+var projectType = ""
+var hourlyRate = 0
+var dueDate = ""
+var dueDays = ""
+var totalEarnings = 0
